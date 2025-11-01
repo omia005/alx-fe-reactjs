aec7cac0035e7bac474f319173827eed81e8ae2e
Evans Omia <evansomia005@gmail.com>
2025-11-02 01:39:48 +0400

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,9 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage.jsx'
-import Header from './components/Header.jsx'
-import Footer from './components/Footer.jsx'
-import MainContent from './components/MainContent.jsx'
+import {Header} from './components/Header.jsx'
+import {Footer} from './components/Footer.jsx'
+import {MainContent} from './components/MainContent.jsx'
 
 
 function App() {
