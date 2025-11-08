cfbcea77f1f09b2dc016dc1051813bd63793e06d
Evans Omia <evansomia005@gmail.com>
2025-11-09 02:16:19 +0400

    Update MainContent.jsx added inline styling

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,7 +1,7 @@
 export default function MainContent(){
   return(
-    <main>
-      <p>I love to visit New York, Paris, and Tokyo.</p>
+    <main style={{padding: "2rem", backgroundColor: "#f5f5f5", borderRadius: "8px",}}>
+      <p style={{color: "#333",lineHeight: "1.6",textAlign: "center"}}>I love to visit New York, Paris, and Tokyo.</p>
     </main>  
   );
 }
