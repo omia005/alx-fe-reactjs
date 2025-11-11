cbe2c2421d7aa40d12d1710009463d51fbe2f055
Evans Omia <evansomia005@gmail.com>
2025-11-12 02:38:09 +0400

    implemented the UserContext

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -7,7 +7,7 @@ import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
 import MainContent from './components/MainContent.jsx'
 import UserProfile from './components/UserProfile.jsx'
-
+import UserContext from './components/UserContext.js'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -37,7 +37,9 @@ function App() {
       <WelcomeMessage />
       <Header />
       <MainContent />
+      <UserContext.Provider>
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+      </UserContext.Provider>
       <Footer />
     </>
   )
