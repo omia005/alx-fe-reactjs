9faef1a2c37e1ddaee2839ed7efa5691d92126cd
Evans Omia <evansomia005@gmail.com>
2025-11-12 12:58:23 +0400

    Create navbar.jsx

---
+++ my-company/src/navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+function Navbar(){
+  return (
+    <nav>
+      <ul style = {{ listStyle: 'none', display: 'flex', gap: '20px', margin: 0, padding: 0 }}>
+        <li>
+          <link to="/" style={{ textDecoration: 'none', color: 'blue' }}>Home</link>
+        </li>
+        <li>
+          <link to="/About" style={{ textDecoration: 'none', color: 'blue' }>About</link>
+        </li>
+        <li>
+          <link to="/Services" style={{ textDecoration: 'none', color: 'blue' }>Services</link>
+        </li>
+        <li>
+          <link to="/Contact" style={{ textDecoration: 'none', color: 'blue' }>Contacts</link>
+        </li>
+      </ul> 
+    </nav>
+  );
+}
+
+export default Navbar;            
