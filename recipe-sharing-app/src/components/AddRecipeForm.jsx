d6930b870262b8db2afeab50559bfd507ecf49fb
Evans Omia <evansomia005@gmail.com>
2025-11-17 00:59:11 +0400

    Update AddRecipeForm.jsx

--- recipe-sharing-app/src/components/AddRecipeForm.jsx
+++ recipe-sharing-app/src/components/AddRecipeForm.jsx
@@ -2,7 +2,7 @@
   import { useState } from 'react';
   import { useRecipeStore } from './recipeStore';
 
-  const AddRecipeForm = () => {
+  export const AddRecipeForm = () => {
     const addRecipe = useRecipeStore(state => state.addRecipe);
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
