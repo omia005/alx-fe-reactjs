2b9b16bc1a1f5ea5acc06ecfd613e97ad921123b
Evans Omia <evansomia005@gmail.com>
2025-11-17 00:55:57 +0400

    Update App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import AddRecipeForm './components/AddRecipeForm.jsx'
+import RecipeList from './components/RecipeList.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
