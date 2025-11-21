2ffcf1e94d2f8cf304c6bfc9056ed161e38fed3a
Evans Omia <evansomia005@gmail.com>
2025-11-21 15:46:31 +0400

    updated App.jsx

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -1,14 +1,9 @@
  import  useRecipeStore  from './recipeStore';
+ import EditRecipeForm from './EditRecipeForm'
 
   const RecipeDetails = ({ recipeId }) => {
     const recipe = useRecipeStore(state =>
       state.recipes.find(recipe => recipe.id === recipeId)
-    );
-    const updateRecipe = useRecipeStore(state => 
-      state.updateRecipe
-    );
-    const deleteRecipe = useRecipeStore(state =>
-      state.deleteRecipe
     );
 
     if (!recipe) {
@@ -19,9 +14,8 @@
       <div>
         <h1>{recipe.title}</h1>
         <p>{recipe.description}</p>
-        <button onClick = {() => deleteRecipe(recipe.id)}>Delete Recipe</button>
-        <button onClick = {() => updateRecipe({...recipe, title: recipe.title + '(upated)'})}
-        >Update Recipe</button>
+        <EditRecipeForm recipeId={recipe.id} />
+        <DeleteRecipeButton recipeId={recipe.id} />
       </div>
     );
   };
