b91a2cbcaf398295a49bdac3f020064efbaea0f5
Evans Omia <evansomia005@gmail.com>
2025-11-21 10:21:05 +0400

    debugged some errors

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {AddRecipeForm} from './components/AddRecipeForm.jsx'
-import {RecipeList} from './components/RecipeList.jsx'
+import AddRecipeForm from './components/AddRecipeForm.jsx'
+import RecipeList from './components/RecipeList.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
