3a71a05a1e91e0c1c9451a378008bd93a2fe7380
Evans Omia <evansomia005@gmail.com>
2025-11-22 15:07:55 +0400

    updated Search.jsx

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -17,7 +17,7 @@ function Search() {
       const userData = await fetchUserData(query);
       setUser(userData);
     } catch (err) {
-      setError("Looks like we can't find the user");
+      setError("Looks like we cant find the user");
     } finally {
       setLoading(false);
     }
