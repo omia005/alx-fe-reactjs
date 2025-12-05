64100f9bbd9a8e91da7e30284c43098d6cabb5c3
Evans Omia <evansomia005@gmail.com>
2025-12-05 16:12:55 +0400

    Update and rename homepage.jsx to HomePage.jsx

---
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -0,0 +1,36 @@
+import {useState} from 'react';
+import {useEffect} from 'react';
+
+
+const HomePage = () =>{
+   const [recipe, setRecipe] = useState([])
+
+    useEffect(() => {
+        fetch('src/data.json')
+        .then((response) => response.json())
+        .then((data) => setRecipe(data))
+    }, []);
+
+    return (
+        <div className="p-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+         {recipe.map((recipe) => (
+         <div
+            key={recipe.id}
+            className="bg-white rounded-2xl shadow p-4 hover:shadow-lg transition"
+         >
+         <img
+          src={recipe.image}
+          alt={recipe.title}
+          className="w-full h-40 object-cover rounded-xl mb-4"
+         />
+         <h2 className="text-xl font-semibold mb-2">{recipe.title}</h2>
+         <p className="text-gray-600 text-sm">{recipe.summary}</p>
+        </div>
+      ))}
+        </div>
+);
+}
+export default Homepage;
+
+   
+  
