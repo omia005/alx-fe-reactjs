5f537f17524c2760b787c3f03ba6482492735ea6
Evans Omia <evansomia005@gmail.com>
2025-12-05 15:15:19 +0400

    made the page a responsive design by adding md

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -12,7 +12,7 @@ const HomePage = () =>{
     }, []);
 
     return (
-        <div className="p-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="p-6 grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 gap-6">
          {recipe.map((recipe) => (
          <div
             key={recipe.id}
