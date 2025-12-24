b5204818d823a5771ab74a45a8caa80007842661
Evans Omia <evansomia005@gmail.com>
2025-12-24 20:19:14 +0400

    created ProtectedRoutes.jsx

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,8 +1,15 @@
 import { Navigate } from "react-router-dom";
 
-const isAuthenticated = true; // simulate login status
+
+function useAuth() {
+  // Simulate authentication status
+  const isAuthenticated = true; // change to false to test redirect
+  return { isAuthenticated };
+}
 
 export default function ProtectedRoute({ children }) {
+  const { isAuthenticated } = useAuth();
+
   if (!isAuthenticated) {
     return <Navigate to="/login" />;
   }
