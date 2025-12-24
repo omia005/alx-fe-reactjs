48941d58249d55831f22c02a73678ae072023d61
Evans Omia <evansomia005@gmail.com>
2025-12-24 20:12:42 +0400

    created Profile.jsx

---
+++ react-router-advanced/src/components/Profile.jsx
@@ -0,0 +1,16 @@
+import { Outlet, Link } from "react-router-dom";
+
+export default function Profile() {
+  return (
+    <div>
+      <h2>Profile</h2>
+
+      <nav>
+        <Link to="">Details</Link> |{" "}
+        <Link to="settings">Settings</Link>
+      </nav>
+
+      <Outlet />
+    </div>
+  );
+}
