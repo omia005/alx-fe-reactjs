32854ce0288705a64d7a6e3093fc0ab9b0f1837e
Evans Omia <evansomia005@gmail.com>
2025-12-24 19:10:14 +0400

    refactored the registrationform using formik

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -1,28 +1,4 @@
-import { useState } from "react";
-
-export default function RegistrationForm() {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [errors, setErrors] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    if (!username) {
-      setErrors("Username is required");
-      return;
-    }
-
-    if (!email) {
-      setErrors("Email is required");
-      return;
-    }
-
-    if (!password) {
-      setErrors("Password is required");
-      return;
-    }
+i
 
     setErrors("");
     console.log({ username, email, password });
